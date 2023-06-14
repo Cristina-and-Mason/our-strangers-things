ea4ae9b69773a90cfc7a1d7982cf36a1fc5f93bd
Cristina Gutierrez <cristinagutierrez@Cristinas-MacBook-Pro.local>
2023-06-13 19:42:20 -0500

    added postslist and index file and added the fetching code to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {Routes, Route, Link} from 'react-router-dom'
+import { PostsList } from './components/index';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Step S: Setup
+  const [allPosts, setAllPosts] =useState ([]);
+
+  // Step F: Fetch our data
+  useEffect (() =>{
+    async function fetchOurPosts(){
+      try {
+          const resonse= await fetch("https://strangers-things.herokuapp.com/api/2304-FTB-ET-WEB-FT/posts`")
+
+          const translatedData= await response.json();
+
+          setAllPosts (translatedData.posts);
+
+          console.log(translatedData.posts);
+      }catch (error){
+        console.log(error);
+      }
+    }
+    fetchOurPosts ();
+  }, [])
 
   return (
     <>
-      
+        <h2>Hello!</h2>
+      <PostsList allPosts={allPosts} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<PostsList />} />
+          <Route path="/posts/:id" element={<SinglePost />} />
+        </Routes>
     </>
   )
 }
