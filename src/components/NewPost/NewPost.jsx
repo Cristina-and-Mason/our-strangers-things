9684fbcb2ea192548fdbc88ec64565b233a4b739
mwlordmason <mason.walker1709@gmail.com>
2023-06-15 00:50:40 -0600

    partially did newPost and fixed some routes

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,9 +1,24 @@
 function NewPost (props){
-    console.log(props)
+    // console.log(props)
     return(
-        <div>
-            <h2>Single Post:</h2>
-        </div>
+        <form onSubmit={(event) =>{
+            event.preventDefault();
+        }}>
+            <label>New Post Title:</label>
+            <input 
+            title="title" 
+            type="text" 
+            placeholder="Your title goes here"
+            value={props.newPost}
+            onChange={(event) => {
+                props.setNewPost(event.target.value)
+                console.log(event.target.value)
+            }}
+            ></input><br/>
+            <label>New Post Description:</label>
+            <input name="description" type="text" placeholder="Your description goes here"></input><br/>
+            <button type="submit">Create New Post</button>
+        </form>
     )
 }
 
