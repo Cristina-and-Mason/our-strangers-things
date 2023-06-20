7f86a0c42cefbf7273a5390eb113a2c76f9c6485
mwlordmason <mason.walker1709@gmail.com>
2023-06-19 18:35:13 -0600

    mason part is done, CSS is all thats left

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -1,18 +1,11 @@
-import { useNavigate } from "react-router-dom";
 function SinglePost ( props ){
-    const navigate = useNavigate()
-    const messagePage = ()=>{
-        navigate(`/posts/${props.id}/messages`)
-    } 
-    
     return(
-            <form onSubmit={messagePage}>
+            <form>
             <h2>Title: {props.title}</h2>
             <h2>ID# {props.id}</h2>
             <h2>Description: {props.description}</h2>
             <h2>Author: {props.author.username}</h2>
             <h2>Price: {props.price}</h2>
-            <button type="submit">Send a message</button>
             </form> 
     )
 }
